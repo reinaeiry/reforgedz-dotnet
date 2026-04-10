8d5b4cfb01e4ef1f154062a36834162f7aa0707e
Eiry <heenanr@outlook.com>
2026-04-10 20:16:32 +0100

    x

--- public/script.js
+++ public/script.js
@@ -120,13 +120,11 @@ function renderServers(data) {
   serversGrid.innerHTML = data.servers.map(srv => {
     const tag = shortName(srv.name);
     const map = mapName(tag);
-    const info = srv.state === 'running' && srv.uptime ? `Online \u00b7 ${srv.uptime}` : stateLabel(srv.state);
     return `<div class="status-tile ${srv.state} reveal visible">
-      <span class="status-region">${srv.region}</span>
       <span class="status-dot ${srv.state}"></span>
       <span class="status-tag">${tag}</span>
       <span class="status-map">${map}</span>
-      <span class="status-info ${srv.state}">${info}</span>
+      <span class="status-info ${srv.state}">${stateLabel(srv.state)}</span>
     </div>`;
   }).join('');
 
